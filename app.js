d9db6820850d24cf85e8f707917750cd06a2126c
Ferran01~ <alumno.398783@ies-azarquiel.es>
2022-11-11 16:01:36 +0100

    first commit

--- app.js
+++
@@ -1,9 +0,0 @@
-let cart = document.querySelector('.cart-items-container');
-
-document.querySelector('#cesta').onmouseover = () =>{
-    cart.classList.add('active');
-}
-
-document.querySelector('#cruz').onclick = () =>{
-    cart.classList.remove('active');
-}
